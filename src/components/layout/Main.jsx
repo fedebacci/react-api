6a32199978dd647b1035aa2904ca9fafebb67096
fedebacci <federicobacci1999@gmail.com>
2025-05-28 16:08:09 +0200

    aggiunta della lista di attori (separatamente)

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -2,28 +2,44 @@ import { useState, useEffect } from "react";
 import axios from "axios"
 
 
-const apiBaseUrl = 'https://lanciweb.github.io/demo/api/actresses/';
+const apiActressesUrl = 'https://lanciweb.github.io/demo/api/actresses/';
+const apiActorsUrl = 'https://lanciweb.github.io/demo/api/actors/';
 
 
 export default function Main () {
 
     const [actresses, setActresses] = useState([])
+    const [actors, setActors] = useState([])
 
     useEffect(() => {
         console.debug('CREAZIONE ELEMENTO MAIN, RICHIEDO DATI INIZIALI AD API');
 
         axios
-            .get(apiBaseUrl)
+            .get(apiActressesUrl)
             .then(response => {
                 console.log(response.data);
 
-                setActresses(response.data)
+                setActresses(response.data);
+
+                axios
+                    .get(apiActorsUrl)
+                    .then(response => {
+                        console.log(response.data);
+                        setActors(response.data);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    })
+                    .finally(() => {
+                        console.info("Seconda richiesta terminata")
+                    });
+
             })
             .catch(err => {
                 console.error(err);
             })
             .finally(() => {
-                console.info("Richiesta terminata")
+                console.info("Prima richiesta terminata")
             });
     }, [])
 
@@ -33,6 +49,11 @@ export default function Main () {
 
             <div className="container my-5">
                 <div className="row row-cols-4 g-3">
+                    <div className="col-12">
+                        <h2>
+                            Actresses
+                        </h2>
+                    </div>
                     {
                         actresses.map(actress => {
                             return (
@@ -54,7 +75,10 @@ export default function Main () {
                                             {actress.biography}
                                             <br />
                                             <br />
+                                            Awards:
+                                            <br />
                                             {actress.awards}
+                                            <br />
                                             <br />
                                             Most famous films:
                                             <ul>
@@ -63,6 +87,69 @@ export default function Main () {
                                                         return (
                                                             <li key={index}>
                                                                 {movie}
+                                                            </li>
+                                                        );
+                                                    })
+                                                }
+                                            </ul>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+            <div className="container my-5">
+                <div className="row row-cols-4 g-3">
+                    <div className="col-12">
+                        <h2>
+                            Actors
+                        </h2>
+                    </div>
+                    {
+                        actors.map(actor => {
+                            return (
+                                <div className="col" key={actor.id}>
+                                    <div className="card shadow h-100">
+                                        <div className="card-header">
+                                            <img className="img-fluid" src={actor.image} alt={actor.name} />
+                                        </div>
+                                        <div className="card-body">
+                                            <strong>
+                                                {actor.name}
+                                            </strong>
+                                            <br />
+                                            <em>
+                                                {actor.nationality} - {actor.birth_year} {actor.death_year ? " (" + actor.death_year + ")" : ""}
+                                            </em>
+                                            <br />
+                                            <br />
+                                            {actor.biography}
+                                            <br />
+                                            <br />
+                                            Awards:
+                                            <br />
+                                            {
+                                                actor.awards.map((award, index) => {
+                                                    return (
+                                                        <>
+                                                            <span key={index}>{award}</span> 
+                                                            {index !== actor.awards.length - 1 ? ", " : ""}
+                                                        </>
+                                                    );
+                                                })
+                                            }
+                                            <br />
+                                            <br />
+                                            Most famous films:
+                                            <ul>
+                                                {
+                                                    actor.known_for.map((movie, index) => {
+                                                        return (
+                                                            <li key={index}>
+                                                                {movie} {""}
                                                             </li>
                                                         );
                                                     })
